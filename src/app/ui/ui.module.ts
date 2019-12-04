39a9b4652319250a570bbb84c3a99586e380b893
Leonardo Wiest <leonardowiest@hotmail.com>
2019-12-04 15:16:57 -0300

    Create project

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { LayoutComponent } from "./layout/layout.component";
+
+@NgModule({
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [LayoutComponent]
+})
+export class UiModule {}
